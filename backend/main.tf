81862085f4db7f0a43e7f46014d1b36ec3d3a763
Abanoub-01 <nobamaroo2013@gmail.com>
2025-06-09 18:18:07 +0300

    Changes to be committed:

--- backend/main.tf
+++ backend/main.tf
@@ -28,16 +28,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "terraform-locks"
-  hash_key       = "LockID"
-  billing_mode   = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name           = "terraform-locks"
+#   hash_key       = "LockID"
+#   billing_mode   = "PAY_PER_REQUEST"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags = {
-    Name = "terraform-locks"
-  }
-}
+#   tags = {
+#     Name = "terraform-locks"
+#   }
+# }
